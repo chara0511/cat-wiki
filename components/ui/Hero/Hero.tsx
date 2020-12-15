371a905577b070c2f0ea36fecd24ad3441487698
Chara- <escor05_11@hotmail.com>
2020-12-14 19:36:10 -0500

    mobile first: hero header done

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -1,23 +1,35 @@
 import Link from 'next/Link'
 import Image from 'next/image'
-import s from './Hero.module.css'
+import CatwikiLogo from 'public/CatwikiLogo'
+import s from './Hero.module.scss'
 
 const Hero = () => {
   return (
-    <div className={s.container}>
-      <div>
+    <div className={s.content}>
+      <div className={s.header}>
         <Image
-          className={s.heroBackground}
+          className={s.headerBackground}
           src="/HeroImagesm.png"
           alt="Picture of the hero"
           width={375}
           height={180}
         />
-        <p>Get to know more about your cat breed</p>
-        <input type="text" placeholder="search" />
+
+        <div className={s.headerContent}>
+          <span>
+            <CatwikiLogo />
+          </span>
+
+          <p>Get to know more about your cat breed</p>
+
+          <div>
+            <input type="text" placeholder="Search" />
+            <span>i</span>
+          </div>
+        </div>
       </div>
 
-      <div>
+      <div className={s.body}>
         <span>Most Searched Breeds</span>
         <h1>66+ Breeds For you to discover</h1>
         <Link href="/breeds/popular">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+import '../styles/index.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
